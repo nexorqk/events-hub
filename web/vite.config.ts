01b85bc6cbb3cda7ded53455de5580db829c1896
nexorqk <pkms2a10@gmail.com>
2026-04-29 14:09:14 +0200

    chore: add react compilter

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,7 +3,14 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+    tailwindcss(),
+  ],
   server: {
     port: 5173,
     host: "127.0.0.1",
